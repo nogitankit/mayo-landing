516b44283e78a597936f54609f13fe3348d33eff
Ankit Sharma <as3006533@gmail.com>
2025-06-30 18:02:00 +0530

    smol error

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-const repoName = "mayo-landing";
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? `/${repoName}/`
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+const repoName = "mayo-landing"; // your GitHub repo name
+
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? `/${repoName}/` : "/",
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === "development" && componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
